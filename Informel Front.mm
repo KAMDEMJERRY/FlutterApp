daebd7a92a5b2044fc3936f878b35e2d1750d902
Ndombou jerry <ndomboukamdem@gmail.com>
2025-04-03 22:22:19 +0100

    Gestures

---
+++ Informel Front.mm
@@ -0,0 +1,76 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Informel: Front" FOLDED="false" ID="ID_483218817" CREATED="1743383337646" MODIFIED="1743383661730" BACKGROUND_COLOR="#3366ff" STYLE="oval">
+<font SIZE="16"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="1. Install flutter" POSITION="right" ID="ID_1556702081" CREATED="1743383366271" MODIFIED="1743383573486" HGAP_QUANTITY="46.24999903887513 pt" VSHIFT_QUANTITY="-159.74999523907914 pt" BACKGROUND_COLOR="#ff3399">
+<edge COLOR="#ff0000"/>
+<font NAME="Serif" SIZE="12"/>
+</node>
+<node TEXT="2. Formation sur flutter" POSITION="left" ID="ID_1346702916" CREATED="1743383500355" MODIFIED="1743383649350" BACKGROUND_COLOR="#66ff33">
+<edge COLOR="#00ff00"/>
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</map>
--- lib/main.dart
+++ lib/main.dart
@@ -21,97 +21,39 @@ class MyHomePage extends StatelessWidget {
 
   final String title;
 
+  // user defined function
+  void _showDialog(BuildContext context) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: Text("Message"),
+          content: Text("Hello World"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-        children: [
-          ProductBox(
-                    name: "iPhone", 
-                    description: "iPhone is the stylist phone ever", 
-                    price: 1000,
-                    image: "iPhone.png",
-          ),
-          ProductBox(
-                    name: "Pixel",
-                    description: "Pixel is the most featured phone ever",
-                    price: 1000, 
-                    image: "Pixel.png"
-          ),
-          ProductBox(
-            name: "Laptop",
-            description: "Laptop is most productive development tool",
-            price: 2000,
-            image: "Laptop.png"
-          ),
-          ProductBox(
-            name: "Tablet",
-            description: "Tablet is the most useful device ever for meeting",
-            price: 1500,
-            image: "Tablet.png"
-          ),
-          ProductBox(
-          name: "Pendrive",
-          description: "Pendrive is useful storage medium",
-          price: 100,
-          image: "Pendrive.png"),
-          ProductBox(
-          name: "Floppy Drive",
-          description: "Floppy drive is useful rescue storage medium",
-          price: 20,
-          image: "Floppy.png"),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-class ProductBox extends StatelessWidget {
-  const ProductBox({
-    super.key,
-    required this.name,
-    required this.description,
-    required this.price,
-    required this.image,
-  });
-
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(2),
-      height: 120,
-      child: Card(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset("assets/appimages/$image"),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text(description),
-                    Text("Price : $price"),
-                  ],
-                ),
-              ),
-            ),
-          ], 
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            _showDialog(context);
+          },
+          child: Text('Hello World'),
         ),
       ),
     );
